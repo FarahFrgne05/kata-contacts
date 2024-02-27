86c307eaf535152ebd838f3ca98f92430d6b55ae
FarahFrgne05 <farah.ferguene@efrei.net>
2024-02-27 13:58:09 +0100

    TODO in ContactsGenerator

--- java/src/main/java/info/dmerej/contacts/ContactsGenerator.java
+++ java/src/main/java/info/dmerej/contacts/ContactsGenerator.java
@@ -1,17 +1,27 @@
 package info.dmerej.contacts;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ContactsGenerator {
   public Stream<Contact> generateContacts(int count) {
-    // TODO: generate a *lot* of contacts instead of just 3
-    Contact[] contacts = new Contact[]{
-      new Contact("Alice", "alice@aol.com"),
-      new Contact("Bob", "bob@gmail.com"),
-      new Contact("Eve", "eve@fastmail.com"),
-    };
+
+    Contact[] contacts = new Contact[count];
+
+    for(int i = 0; i<count; ++i){
+      String name = "Alice"+i;
+      String email = "alice"+i+"@aol.com";
+      Contact contact = new Contact(name, email);
+      contacts[i] = contact;
+    }
+
+
+      //new Contact("Alice", "alice@aol.com"),
+      //new Contact("Bob", "bob@gmail.com"),
+      //new Contact("Eve", "eve@fastmail.com"),
+
     return Arrays.stream(contacts);
   }
 }
